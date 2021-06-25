702d620be57bd00085a01940d0ab4461e98f94ea
YevhenTsilenko <fiboss1992@gmail.com>
2021-06-25 15:34:06 +0300

    color switcher

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
+
+const startBtn = document.querySelector('[data-action="start"]');
+const stopBtn = document.querySelector('[data-action="stop"]');
+const body = document.body;
+// console.log(startBtn);
+startBtn.addEventListener('click' , onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+let isStarted = false;
+let timerId;
+const randomIntegerFromInterval = (min, max) => {
+    min = 0;
+    max = colors.length - 1;
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+
+function colorChange () {
+    body.style.backgroundColor = colors[randomIntegerFromInterval()];
+}
+
+function onStartClick() {
+    if(isStarted) {
+        return;
+    }
+    isStarted = true;
+
+    timerId = setInterval(colorChange, 1000);
+}
+
+function onStopClick () {
+    if(!isStarted) {
+        return;
+    }
+    isStarted = false;
+    clearInterval(timerId);
+}
+
+
+
+
